b00f4453581cfed3339018bcfc52af837186bba5
Vitaliy Kononenko <v.o.kononenko@gmail.com>
2019-10-15 16:16:48 +0300

    Remove invalid imprort group

--- src/main/kotlin/io/konv/quarkus/entities/Person.kt
+++ src/main/kotlin/io/konv/quarkus/entities/Person.kt
@@ -1,14 +1,8 @@
 package io.konv.quarkus.entities
 
-import javax.persistence.*
-
 class Person {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "ID")
     var id: Long = 0
 
-    @Column(name = "NAME")
     var name: String = ""
 }
